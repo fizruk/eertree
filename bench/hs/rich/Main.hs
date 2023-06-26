27ed8e3e78d28ef44313167308b19489a591088b
PATH242 <mennashaheen2357@gmail.com>
2023-06-26 01:24:53 +0100

    Minor fixes to rich strings benchmarks

--- bench/hs/rich/Main.hs
+++ bench/hs/rich/Main.hs
@@ -1,10 +1,8 @@
--- import           Data.Proxy      (Proxy (..))
+import Criterion.Main
 import           EERTREE.Simple
 
-import           Criterion.Main
-
 benchmark :: [Int] -> Benchmark
-benchmark inputValues = bgroup "a216264" [ bench (show n) $ whnf a216264 n | n <- inputValues ]
+benchmark inputValues = bgroup "a216264" [bench (show n) $ nf (show . a216264) n | n <- inputValues]
 
 main :: IO ()
 main = defaultMain [benchmark [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22]]
