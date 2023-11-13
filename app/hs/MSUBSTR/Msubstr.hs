acc07639d74f54438da10fd66ce2c827eb1fd2cb
PATH242 <mennashaheen2357@gmail.com>
2023-11-13 17:21:19 +0300

    msubstr bench

--- app/hs/MSUBSTR/Msubstr.hs
+++ app/hs/MSUBSTR/Msubstr.hs
@@ -7,21 +7,37 @@
 {-# LANGUAGE TypeApplications    #-}
 
 module Main where
-
+import Data.Foldable as F
 import Data.List (maximumBy)
 import Data.Ord (comparing)
-import           EERTREE.Applications
-import           EERTREE.Simple
+import           EERTREE.List
+import           EERTREE.Node
+countMax :: Ord a => a -> [a] -> (a, Int)
+countMax z = foldl' step (z, 1)
+  where
+    step acc@(x, n) y
+      | y > x = (y, 1)
+      | y == x = (x, n + 1)
+      | otherwise = acc
+
+countMax1 :: Ord a => [a] -> (a, Int)
+countMax1 [] = error "empty list"
+countMax1 (x:xs) = countMax x xs
+
+getMsubstr :: String -> (Int, Int)
+getMsubstr str = countMax1 (map len (palindromes tree))
+  where
+    tree = eertreeFromString @26 str
 
 processMsubstrLine :: Int -> IO()
 processMsubstrLine 0 = return ()
 processMsubstrLine x = do
     str <- getLine
-    let (lenn, freq) = (getMsubstr str)
+    let (lenn, freq) = getMsubstr str
     putStrLn(show lenn ++ " " ++ show freq)
     processMsubstrLine (x - 1)
 
 main :: IO ()
 main = do
   t <- readLn @Int
-  (processMsubstrLine t)
+  processMsubstrLine t
